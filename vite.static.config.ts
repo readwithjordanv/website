8df987faf7667cb7de0d3ca2699b45156febe812
Testing <testing@Mac.lan>
2026-04-29 14:42:37 +1000

    /website removal

--- vite.static.config.ts
+++ vite.static.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/website/',
+    base: '/',
     plugins: [
         tailwindcss(),
         vue({
